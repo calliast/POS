ec853667af842ee5f142bc221f0a217c40bd293e
calliast <muharipan@gmail.com>
2022-11-19 10:09:39 +0700

    3rd - added templatecss and html

--- c23/app.js
+++ c23/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session")
+var flash = require('connect-flash')
 
 async function main() {
   try {
@@ -39,6 +41,13 @@ main().then((db) => {
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, "public")));
+  app.use(session({
+    secret: 'rubicamp',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+  }))
+  app.use(flash())
 
   app.use("/", indexRouter);
   app.use("/users", usersRouter);
--- c23/routes/index.js
+++ c23/routes/index.js
@@ -1,24 +1,97 @@
 var express = require("express");
 var router = express.Router();
+const bcrypt = require("bcrypt");
+const helpers = require("../helpers/util");
 
 module.exports = function (db) {
   /* GET home page. */
-  router
-  .route("/")
-  .get(async function (req, res) {
+  router.route("/").get(helpers.isLoggedIn, async function (req, res) {
     try {
       // let sql = `SELECT "userid", "email", "name", "password", "role" FROM users ORDER BY "userid" ASC`;
       let sql = `SELECT * FROM users`;
       const readData = await db.query(sql);
       // const readData = `test`
-      res.render("index", { 
-        title: "Express", 
-        data: readData.rows
+      res.render("./sb-admin2/index", {
+        title: "Express",
+        data: readData.rows,
+        user: req.session.user,
       });
     } catch (error) {
-      res.send('error')
+      res.send("error");
     }
   });
+
+  router.route("/logout").get(async function (req, res) {
+    await req.session.destroy();
+    res.redirect("/login");
+  });
+
+  router
+    .route("/login")
+    .get(async function (req, res) {
+      res.render("./sb-admin2/login", { info: req.flash(`info`) });
+    })
+    .post(async function (req, res) {
+      try {
+        let sql = `SELECT * FROM users WHERE email = $1`;
+        const { email, password } = req.body;
+
+        const checkEmail = await db.query(sql, [email]);
+        if (checkEmail.rows.length == 0) {
+          req.flash(`info`, `Email tidak terdaftar`);
+          return res.redirect("/login");
+        }
+
+        const checkPassword = await bcrypt.compare(
+          password,
+          checkEmail.rows[0].password
+        );
+        if (!checkPassword) {
+          req.flash(`info`, `Password Salah!`);
+          return res.redirect('/login');
+        }
+
+        req.session.user = checkEmail.rows[0];
+        res.redirect('/');
+      } catch (error) {
+        res.send(error);
+      }
+    });
+
+  router
+    .route("/register")
+    .get(async function (req, res) {
+      res.render("./sb-admin2/register", { info: req.flash(`info`) });
+    })
+    .post(async function (req, res) {
+      try {
+        let sql = `SELECT * FROM users where email = $1`;
+        const { name, email, password } = req.body;
+        const role = "admin";
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const checkData = await db.query(sql, [email]);
+        console.log(checkData.rows);
+        if (checkData.rowCount > 0) {
+          req.flash(`info`, `Email sudah terdaftar`);
+          return res.redirect('/register');
+        }
+
+        sql = `INSERT INTO users("email", "name", "password", "role") VALUES ($1, $2, $3, $4)`;
+
+        const insertData = await db.query(sql, [
+          email,
+          name,
+          hashedPassword,
+          role,
+        ]);
+
+        res.redirect("/login");
+      } catch (error) {
+        res.send(error);
+      }
+    });
 
   return router;
 };
